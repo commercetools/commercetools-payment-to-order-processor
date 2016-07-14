cd2211056157d9768aa17185d2e0533018e04dd6
mht-dotsource <mht@dotsource.de>
2016-07-14 11:26:40 +0200

    added spring boot batch skeleton with basic ctp message reading

---
+++ src/main/java/com/commercetools/paymenttoorderprocessor/ShereClientConfiguration.java
@@ -0,0 +1,33 @@
+package com.commercetools.paymenttoorderprocessor;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ShereClientConfiguration {
+
+    @Value("${ctp.credentials.clientid}")
+    private String clientId;
+    @Value("${ctp.credentials.clientsecret}")
+    private String clientSecret;
+    @Value("${ctp.credentials.projectkey}")
+    private String projectKey;
+    @Value("${ctp.defaulttimeout}")
+    private Integer defaultTimeout;
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public String getClientSecret() {
+        return clientSecret;
+    }
+
+    public String getProjectKey() {
+        return projectKey;
+    }
+    public Integer getDefaultTimeout() {
+        return defaultTimeout;
+    }
+    
+}
