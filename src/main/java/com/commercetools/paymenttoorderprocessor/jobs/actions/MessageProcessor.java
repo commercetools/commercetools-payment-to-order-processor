6ae625b8eb6c002c203a382bb98f42add84aef9f
mht-dotsource <mht@dotsource.de>
2016-07-14 16:25:11 +0200

    filter messages by transactionstatus

--- src/main/java/com/commercetools/paymenttoorderprocessor/jobs/actions/MessageProcessor.java
+++ src/main/java/com/commercetools/paymenttoorderprocessor/jobs/actions/MessageProcessor.java
@@ -3,13 +3,24 @@ package com.commercetools.paymenttoorderprocessor.jobs.actions;
 import org.springframework.batch.item.ItemProcessor;
 
 import io.sphere.sdk.messages.Message;
+import io.sphere.sdk.payments.TransactionState;
+import io.sphere.sdk.payments.messages.PaymentTransactionStateChangedMessage;
 
 
 public class MessageProcessor implements ItemProcessor<Message, Message> {
     @Override
     public Message process(Message message) throws Exception {
-        //TODO Filter Messages
-        return message;
+        return filter(message);
     }
 
+    private PaymentTransactionStateChangedMessage filter(final Message message) {
+        if (message instanceof PaymentTransactionStateChangedMessage){
+            final PaymentTransactionStateChangedMessage paymentTransactionStateChangedMessage = (PaymentTransactionStateChangedMessage)message;
+            if (TransactionState.SUCCESS.equals(paymentTransactionStateChangedMessage.getState())) {
+                return paymentTransactionStateChangedMessage;
+            }
+        }
+
+        return null;
+    }
 }
