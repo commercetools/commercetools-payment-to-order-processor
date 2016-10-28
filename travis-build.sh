1eed0e87befae124ed233aa1e66c98c726efd0a4
Andrii Kovalenko <andrii.kovalenko@commercetools.de>
2016-10-28 14:57:18 +0200

    added missing environment variable SHORT_COMMIT

--- travis-build.sh
+++ travis-build.sh
@@ -8,23 +8,23 @@ export DOCKER_TAG=`if [ "$TRAVIS_BRANCH" == "master" -a "$TRAVIS_PULL_REQUEST" =
 # used for debugging the build, may be suppressed in production
 echo TRAVIS_BUILD_NUMBER=$TRAVIS_BUILD_NUMBER
 echo TRAVIS_TAG=$TRAVIS_TAG
-echo COMMIT=$COMMIT
+echo SHORT_COMMIT=$SHORT_COMMIT
 echo REPO=$REPO
 echo DOCKER_TAG=$DOCKER_TAG
 echo DOCKER_USERNAME=$DOCKER_USERNAME
 
-echo "Building Docker image using tag '${REPO}:${COMMIT}'."
-docker build -t "${REPO}:${COMMIT}" .
+echo "Building Docker image using tag '${REPO}:${SHORT_COMMIT}'."
+docker build -t "${REPO}:${SHORT_COMMIT}" .
 
 docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
 
-echo "Adding additional tag '${REPO}:${DOCKER_TAG}' to already built Docker image '${REPO}:${COMMIT}'."
-docker tag $REPO:$COMMIT $REPO:$DOCKER_TAG
-echo "Adding additional tag '${REPO}:travis-${TRAVIS_BUILD_NUMBER}' to already built Docker image '${REPO}:${COMMIT}'."
-docker tag $REPO:$COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER
+echo "Adding additional tag '${REPO}:${DOCKER_TAG}' to already built Docker image '${REPO}:${SHORT_COMMIT}'."
+docker tag $REPO:$SHORT_COMMIT $REPO:$DOCKER_TAG
+echo "Adding additional tag '${REPO}:travis-${TRAVIS_BUILD_NUMBER}' to already built Docker image '${REPO}:${SHORT_COMMIT}'."
+docker tag $REPO:$SHORT_COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER
 if [ "$TRAVIS_TAG" ]; then
-  echo "Adding additional tag '${REPO}:${TRAVIS_TAG}' to already built Docker image '${REPO}:${COMMIT}'."
-  docker tag $REPO:$COMMIT $REPO:${TRAVIS_TAG};
+  echo "Adding additional tag '${REPO}:${TRAVIS_TAG}' to already built Docker image '${REPO}:${SHORT_COMMIT}'."
+  docker tag $REPO:$SHORT_COMMIT $REPO:${TRAVIS_TAG};
 fi
 echo "Pushing Docker images to repository '${REPO}' (all local tags are pushed)."
 docker push $REPO
