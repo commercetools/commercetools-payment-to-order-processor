36d28db2ff93d5acef526bde8b2fed3cdc4816e0
Andrii Kovalenko <andrii.kovalenko@commercetools.de>
2016-11-21 16:01:46 +0100

    fixed spring migration dependency (exclude DataSourceAutoConfiguration)

--- src/main/java/com/commercetools/paymenttoorderprocessor/PaymentToOrderApplication.java
+++ src/main/java/com/commercetools/paymenttoorderprocessor/PaymentToOrderApplication.java
@@ -1,32 +1,34 @@
 package com.commercetools.paymenttoorderprocessor;
 
-import java.util.concurrent.TimeUnit;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.DependsOn;
-
 import io.sphere.sdk.client.BlockingSphereClient;
 import io.sphere.sdk.client.SphereAccessTokenSupplier;
 import io.sphere.sdk.client.SphereClient;
 import io.sphere.sdk.client.SphereClientFactory;
 import io.sphere.sdk.http.HttpClient;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class PaymentToOrderApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PaymentToOrderApplication.class, args).close();
     }
-    
-    @Bean 
+
+    @Bean
     public HttpClient httpClient() {
         return SphereClientFactory.of().createHttpClient();
     }
-    
+
     @Bean
     @DependsOn({"shereClientConfiguration", "httpClient"})
     public BlockingSphereClient blockingSphereClient(final ShereClientConfiguration config, final HttpClient httpClient) {
