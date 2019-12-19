64bf659b32491adcbde7b77ca5fc53733088e0ba
Andreas Halberkamp <>
2019-12-19 13:44:40 +0100

    Using logstash for formatting logs.

--- src/main/java/com/commercetools/paymenttoorderprocessor/PaymentToOrderApplication.java
+++ src/main/java/com/commercetools/paymenttoorderprocessor/PaymentToOrderApplication.java
@@ -5,6 +5,7 @@ import io.sphere.sdk.client.SphereAccessTokenSupplier;
 import io.sphere.sdk.client.SphereClient;
 import io.sphere.sdk.client.SphereClientFactory;
 import io.sphere.sdk.http.HttpClient;
+import org.slf4j.bridge.SLF4JBridgeHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -21,6 +22,7 @@ import java.util.concurrent.TimeUnit;
 public class PaymentToOrderApplication {
 
     public static void main(String[] args) {
+        bridgeJULToSLF4J();
         SpringApplication.run(PaymentToOrderApplication.class, args).close();
     }
 
@@ -39,4 +41,35 @@ public class PaymentToOrderApplication {
 
         return BlockingSphereClient.of(sphereClient, config.getDefaultTimeout(), TimeUnit.MILLISECONDS);
     }
+
+    /**
+     * Routes all incoming j.u.l. (java.util.logging.Logger) records to the SLF4j API. This is done by:
+     * <ol>
+     *     <li>Removing existing handlers attached to the j.u.l root logger.</li>
+     *     <li>Adding SLF4JBridgeHandler to j.u.l's root logger.</li>
+     * </ol>
+     * <p>Why we do the routing?
+     * <p>Some dependencies (e.g. org.javamoney.moneta's DefaultMonetaryContextFactory) log events using the
+     * j.u.l. This causes such logs to ignore the logback.xml configuration which is only
+     * applied to logs from the SLF4j implementation.
+     *
+     */
+    private static void bridgeJULToSLF4J() {
+        SLF4JBridgeHandler.removeHandlersForRootLogger();
+        SLF4JBridgeHandler.install();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
